dfcca48b1c231b357653227038061b270ad0fbf1
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-08-31 16:40:11 -0400

    Update P4.6.1.py

--- Section 4.6/P4.6.1.py
+++ Section 4.6/P4.6.1.py
@@ -2,8 +2,9 @@ from bank_account import BankAccount, CurrentAccount, SavingsAccount
 from customer import Customer
 
 my_customer = Customer('John Doe', '123 Street S', '1999-01-01')
-my_account = BankAccount(my_customer, 12121412412, 1000.0)
+my_account = BankAccount(my_customer, 12121412412, 200, 1000.0)
 
+my_account.withdraw(2000.0)
 
 
 
